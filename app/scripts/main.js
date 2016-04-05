381bb52cb5719045c774c88effb7c89bd1dd366a
Raul Pineda <raul@raulpineda.mx>
2016-04-05 22:07:03 +0200

    Updates cv with Vivino experience

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,37 +1,39 @@
+'use strict';
+
 $(document).ready(function() {
-    scrollIntervalID = setInterval(stickIt, 10);
-    ageInterval = setInterval(setAge, 100);
-    $('.carousel').carousel()
+  $('.carousel').carousel();
 
-    var birthday = moment('1988-12-24T21:00:00-06:00');
-    var oneYear = 31536000;
-    var ageField = $('.js-age');
+  var birthday = moment('1988-12-24T21:00:00-06:00');
+  var oneYear = 31536000;
+  var ageField = $('.js-age');
 
-    $('.js-scroll').click(function(e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top - 50
-        }, 1000);
-    })
+  $('.js-scroll').click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top - 50
+    }, 1000);
+  });
 
-    function setAge() {
-        var now = moment();
-        var years = now.diff(birthday, 'years');
-        var secondsNow = now.diff(moment().format('YYYY'), 'seconds');
-        var elapsedSeconds = secondsNow / oneYear;
-        var age = years + elapsedSeconds;
-        ageField.html(age.toPrecision(6).toString() + ' years');
-    }
+  function setAge() {
+    var now = moment();
+    var years = now.diff(birthday, 'years');
+    var secondsNow = now.diff(moment().format('YYYY'), 'seconds');
+    var elapsedSeconds = secondsNow / oneYear;
+    var age = years + elapsedSeconds;
+    ageField.html(age.toPrecision(6).toString());
+  }
 
-    function stickIt() {
+  function stickIt() {
 
     var hero = $('.identity').offset();
-    heroBottom = hero.top;
+    var heroBottom = hero.top;
 
     if ($(window).scrollTop() >= (heroBottom)) {
-        $('.stickyNav').fadeIn();
+      $('.stickyNav').fadeIn();
     } else {
-         $('.stickyNav').fadeOut();
+      $('.stickyNav').fadeOut();
     }
-    }
+  }
+  setInterval(stickIt, 10);
+  setInterval(setAge, 100);
 });
